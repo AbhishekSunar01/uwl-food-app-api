4205a9b29dfb70bfb4d89b6f935aa9ad30b19ffe
AbhishekSunar01 <a8hishek01@gmail.com>
2026-04-17 21:35:59 +0100

    fix: mock S3Service instead of S3Client in tests

--- src/test/java/com/uwlFood/api/ApiApplicationTests.java
+++ src/test/java/com/uwlFood/api/ApiApplicationTests.java
@@ -1,20 +1,19 @@
 package com.uwlFood.api;
 
+import com.uwlFood.api.service.S3Service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import software.amazon.awssdk.services.s3.S3Client;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class ApiApplicationTests {
 
-	@MockitoBean
-	private S3Client s3Client;
+    @MockitoBean
+    private S3Service s3Service;
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
