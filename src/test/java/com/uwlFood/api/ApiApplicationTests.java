b9bc63f3fa101c87fab843902d31caa9370d8d30
AbhishekSunar01 <a8hishek01@gmail.com>
2026-04-17 21:26:59 +0100

    fix: mock S3Client in tests for CI

--- src/test/java/com/uwlFood/api/ApiApplicationTests.java
+++ src/test/java/com/uwlFood/api/ApiApplicationTests.java
@@ -3,10 +3,15 @@ package com.uwlFood.api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import software.amazon.awssdk.services.s3.S3Client;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class ApiApplicationTests {
+
+	@MockitoBean
+	private S3Client s3Client;
 
 	@Test
 	void contextLoads() {
